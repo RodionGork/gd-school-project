78a839d4e28a5f3ce7533bd307c509b388b7676b
rodion <rodiongork@github.com>
2019-04-07 09:22:57 +0300

    Added console version

--- spring-2019/web/shop.py
+++ spring-2019/web/shop.py
@@ -102,3 +102,83 @@ def load_state():
 def save_state(s):
     with open('state.json', 'w') as f:
         f.write(json.dumps(s))
+
+
+def main():
+    cont = True
+    while cont:
+        line = input('\nshop> ')
+        cont = console_execute_command(line)
+
+
+def console_execute_command(line):
+    ur = line.split(' ', 4)
+    command = ur[0]
+    user = ur[1] if len(ur) > 1 else None
+    if command == 'login':
+        console_login(user)
+    elif command == 'add' and user is not None:
+        item = ur[2] if len(ur) > 2 else ''
+        qty = ur[3] if len(ur) > 3 else '0'
+        console_add(user, item, qty)
+    elif command == 'order' and user is not None:
+        console_order(user)
+    elif command == 'goods':
+        console_goods()
+    elif command == 'basket' and user is not None:
+        console_basket(user)
+    elif command == 'exit':
+        return False
+    else:
+        console_help()
+    return True
+
+
+def console_login(username):
+    if is_registered_user(username):
+        print('Hello, ' + username)
+    else:
+        print('You are not registered!\nYou can browse, but will not be able to order!')
+
+
+def console_add(username, item, qty):
+    res = add_item(username, item, qty)
+    print('Added %s, amount=%s, cost=%s' % (res[0], res[1], res[2] * res[1]))
+
+
+def console_order(username):
+    if not is_registered_user(username):
+        print('Sorry, we do not ship to unregistered users :(')
+        return
+    order_ready(username)
+    print('Your order is shipped, your basked is empty again. Thanks!')
+
+
+def console_basket(user):
+    basket, total = get_basket_and_total(user)
+    print('Your basket:')
+    for item in basket:
+        qty = basket[item][0]
+        price = basket[item][1]
+        print('  %s, amount=%s, price=%s' % (item, qty, price))
+    print('------\n  total: %s$' % total)
+
+
+def console_goods():
+    print('Our goods:')
+    for item, price in get_goods():
+        print('  %s at %s$' % (item, price))
+
+def console_help():
+    print(
+        'Command list:\n' +
+        '  login <username>\n' +
+        '  add <username> <item> <amount>\n' +
+        '  order <username>\n' +
+        '  basket <username>\n' +
+        '  goods\n' +
+        '  exit')
+
+
+if __name__ == '__main__':
+    main()
